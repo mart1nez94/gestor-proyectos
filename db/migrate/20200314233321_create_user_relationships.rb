87d908c09ee3c52d79d3397fb1b20f4924d2edd0
mart1nez94 <armandoomtzz@gmail.com>
2020-03-14 17:47:32 -0600

    Adds relations

--- db/migrate/20200314233321_create_user_relationships.rb
+++ db/migrate/20200314233321_create_user_relationships.rb
@@ -1,6 +1,8 @@
 class CreateUserRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :user_relationships do |t|
+      t.integer :user_id
+      t.integer :project_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_232534) do
+ActiveRecord::Schema.define(version: 2020_03_14_233321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_03_14_232534) do
     t.string "name"
     t.text "description"
     t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_relationships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
