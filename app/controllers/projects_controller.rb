c5195e94324da97707cbfaf1c2df77aa50a53bdb
mart1nez94 <armandoomtzz@gmail.com>
2020-03-16 19:07:36 -0600

    Adds nav with tabs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,5 +7,9 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.project_by_id(params[:id])
+    @tasks = Task.all_tasks(params[:id])
+    @tasks_to_do = Task.to_do(params[:id])
+    @tasks_doing = Task.doing(params[:id])
+    @tasks_done = Task.done(params[:id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  def self.all_tasks(project_id)
+    Task.all.where(tasks: {project_id: project_id})
+  end
+
+  def self.to_do(project_id)
+    Task.all.where(tasks: {project_id: project_id, status_id: 1})
+  end
+
+  def self.doing(project_id)
+    Task.all.where(tasks: {project_id: project_id, status_id: 2})
+  end
+
+  def self.done(project_id)
+    Task.all.where(tasks: {project_id: project_id, status_id: 3})
+  end
 end
