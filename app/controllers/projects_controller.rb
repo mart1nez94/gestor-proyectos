9cc172f588305ecd51df81c3acbaed7cad92ac75
Armando Guadalupe Moreno Martinez <armandoomtzz@gmail.com>
2020-03-22 22:09:58 -0600

    Fixs dashboard layout (#8)
    * Adds more content for projects index
    
    * Adds create a project feature

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,15 +3,20 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.projects_by_user(current_user.id)
+    @tasks = Task.all_tasks_by_user(current_user.id)
   end
 
   def show
     @project = Project.project_by_id(params[:id])
-    @tasks = Task.all_tasks(params[:id])
+    @tasks = Task.all_tasks_by_project(params[:id])
     @tasks_to_do = Task.to_do(params[:id])
     @tasks_doing = Task.doing(params[:id])
     @tasks_done = Task.done(params[:id])
     @users = User.from_this_project(params[:id])
+  end
+
+  def create
+    Project.create_project(current_user.id, project_params)
   end
 
   def update
