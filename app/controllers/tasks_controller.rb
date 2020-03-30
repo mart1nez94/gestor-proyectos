97e1f641a5fdc39a3af3be6fc01de2f49df83f07
mart1nez94 <armandoomtzz@gmail.com>
2020-03-29 19:29:23 -0600

    Adds alert and redirect after update and delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,12 +12,15 @@ class TasksController < ApplicationController
 
   def update
     Task.update_task(params[:id], task_params)
+    redirect_to request.referrer
+    flash[:success] = "Task was updated successful!"
   end
 
   def destroy
     @task = Task.information(params[:id])
     Task.delete_task(@task.id)
     redirect_to "/projects/#{@task.project_id}"
+    flash[:success] = "Task was deleted successful!"
   end
 
   private
